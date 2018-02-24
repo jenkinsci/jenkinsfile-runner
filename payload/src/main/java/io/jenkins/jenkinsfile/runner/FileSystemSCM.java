f68c3974bac028dca2c5a2d9b62aaea96b78c8e3
Kohsuke Kawaguchi <kk@kohsuke.org>
2018-02-24 14:43:26 -0800

    A bit more of SCM implementation is needed to function

--- payload/src/main/java/io/jenkins/jenkinsfile/runner/FileSystemSCM.java
+++ payload/src/main/java/io/jenkins/jenkinsfile/runner/FileSystemSCM.java
@@ -2,15 +2,18 @@ package io.jenkins.jenkinsfile.runner;
 
 import hudson.FilePath;
 import hudson.Launcher;
+import hudson.model.Job;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.scm.ChangeLogParser;
 import hudson.scm.NullChangeLogParser;
+import hudson.scm.PollingResult;
 import hudson.scm.SCM;
 import hudson.scm.SCMRevisionState;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 
@@ -32,5 +35,25 @@ public class FileSystemSCM extends SCM {
     @Override
     public void checkout(@Nonnull Run<?, ?> build, @Nonnull Launcher launcher, @Nonnull FilePath workspace, @Nonnull TaskListener listener, @CheckForNull File changelogFile, @CheckForNull SCMRevisionState baseline) throws IOException, InterruptedException {
         new FilePath(dir).copyRecursiveTo(workspace);
+    }
+
+    /**
+     * Dummy implementation.
+     *
+     * Jenkinsfile Runner doesn't need to do polling, so this method is not needed.
+     */
+    @Override
+    public SCMRevisionState calcRevisionsFromBuild(@Nonnull Run<?, ?> build, @Nullable FilePath workspace, @Nullable Launcher launcher, @Nonnull TaskListener listener) throws IOException, InterruptedException {
+        return SCMRevisionState.NONE;
+    }
+
+    /**
+     * Dummy implementation.
+     *
+     * Jenkinsfile Runner doesn't need to do polling, so this method is not needed.
+     */
+    @Override
+    public PollingResult compareRemoteRevisionWith(@Nonnull Job<?, ?> project, @Nullable Launcher launcher, @Nullable FilePath workspace, @Nonnull TaskListener listener, @Nonnull SCMRevisionState baseline) throws IOException, InterruptedException {
+        return PollingResult.NO_CHANGES;
     }
 }
