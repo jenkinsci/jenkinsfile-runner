ae95748dd7f897104c62667512140f20fcb76660
Evaristo Gutiérrez <egutierrez@cloudbees.com>
2018-12-21 17:42:09 +0100

    [JENKINS-55239] Make FileSystemSCM compliant with databinding.

--- payload/src/main/java/io/jenkins/jenkinsfile/runner/FileSystemSCM.java
+++ payload/src/main/java/io/jenkins/jenkinsfile/runner/FileSystemSCM.java
@@ -1,5 +1,6 @@
 package io.jenkins.jenkinsfile.runner;
 
+import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.Job;
@@ -9,8 +10,11 @@ import hudson.scm.ChangeLogParser;
 import hudson.scm.NullChangeLogParser;
 import hudson.scm.PollingResult;
 import hudson.scm.SCM;
+import hudson.scm.SCMDescriptor;
 import hudson.scm.SCMRevisionState;
 import hudson.util.DirScanner;
+import jenkins.model.Jenkins;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
@@ -22,10 +26,15 @@ import java.io.IOException;
  * @author Kohsuke Kawaguchi
  */
 public class FileSystemSCM extends SCM {
-    private final File dir;
+    private final String dir;
 
-    public FileSystemSCM(File dir) {
+    @DataBoundConstructor
+    public FileSystemSCM(String dir) {
         this.dir = dir;
+    }
+
+    public String getDir() {
+        return this.dir;
     }
 
     @Override
@@ -35,7 +44,7 @@ public class FileSystemSCM extends SCM {
 
     @Override
     public void checkout(@Nonnull Run<?, ?> build, @Nonnull Launcher launcher, @Nonnull FilePath workspace, @Nonnull TaskListener listener, @CheckForNull File changelogFile, @CheckForNull SCMRevisionState baseline) throws IOException, InterruptedException {
-        new FilePath(dir).copyRecursiveTo(new DirScanner.Glob("**/*", null, false), workspace, "**/*");
+        new FilePath(new File(dir)).copyRecursiveTo(new DirScanner.Glob("**/*", null, false), workspace, "**/*");
     }
 
     /**
@@ -57,4 +66,23 @@ public class FileSystemSCM extends SCM {
     public PollingResult compareRemoteRevisionWith(@Nonnull Job<?, ?> project, @Nullable Launcher launcher, @Nullable FilePath workspace, @Nonnull TaskListener listener, @Nonnull SCMRevisionState baseline) throws IOException, InterruptedException {
         return PollingResult.NO_CHANGES;
     }
+
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl) Jenkins.getInstance().getDescriptorOrDie(FileSystemSCM.class);
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends SCMDescriptor<FileSystemSCM> {
+
+        public DescriptorImpl() {
+            super(FileSystemSCM.class, null);
+            load();
+        }
+
+        public String getDisplayName() {
+            return "FileSystemSCM";
+        }
+
+    }
+
 }
