7db287dc25750308fc4a4a3ce66f3d77db9e264f
Francisco Javier Fernandez Gonzalez <fjfernandez@cloudbees.com>
2019-02-11 16:48:58 +0100

    [JENKINS-54352] Adding tests for the demos

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,29 +2,36 @@
 set -e
 
 current_directory=$(pwd)
-test_framework_directory="$current_directory/jenkinsfile-runner-test-framework"
-sh_unit_directory="$test_framework_directory/shunit2"
+test_framework_directory="$current_directory/.jenkinsfile-runner-test-framework"
+working_directory="$test_framework_directory/work"
+
+version="256.0-test"
+jenkinsfile_runner_tag="jenkins-experimental/jenkinsfile-runner-test-image"
+downloaded_cwp_jar="to_update"
 
 . $test_framework_directory/init-jfr-test-framework.inc
 
 oneTimeSetUp() {
-  echo "Initializing the test suit. Ignoring so far"
+  rm -rf "$working_directory"
+  mkdir -p "$working_directory"
+  downloaded_cwp_jar=$(download_cwp "$working_directory")
+  jfr_tag=$(execute_cwp_jar_and_generate_docker_image "$working_directory" "$downloaded_cwp_jar" "$version" "$current_directory/test_resources/cwp-produced-images/config/packager-config.yml" "$jenkinsfile_runner_tag" | grep 'Successfully tagged')
+  execution_should_success "$?" "$jenkinsfile_runner_tag" "$jfr_tag"
 }
 
-setUp() {
-  echo "Initializing the test case. Ignoring so far"
+test_cwp() {
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/cwp-produced-images/test_cwp/Jenkinsfile"
+  jenkinsfile_execution_should_succeed "$?"
 }
 
-test_example() {
-  echo "Executing a test. Test nothing. It is only used to create structure"
+test_databound() {
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/cwp-produced-images/test_databound/Jenkinsfile"
+  jenkinsfile_execution_should_succeed "$?"
 }
 
-oneTimeTearDown() {
-  echo "Cleaning the test suite. Ignoring so far"
+test_parameters() {
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/cwp-produced-images/test_parameters/Jenkinsfile"
+  jenkinsfile_execution_should_succeed "$?"
 }
 
-tearDown() {
-  echo "Cleaning the test case. Ignoring so far"
-}
-
-. $sh_unit_directory/shunit2
+init_framework
