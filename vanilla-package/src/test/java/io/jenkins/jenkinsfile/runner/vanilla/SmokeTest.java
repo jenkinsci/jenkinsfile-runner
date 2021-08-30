2bf72440b9f7fcae394a06f6e3b9ed32f3fad296
Oleg Nenashev <o.v.nenashev@gmail.com>
2021-08-30 10:32:24 +0200

    Make the unit test cross-platform

--- vanilla-package/src/test/java/io/jenkins/jenkinsfile/runner/vanilla/SmokeTest.java
+++ vanilla-package/src/test/java/io/jenkins/jenkinsfile/runner/vanilla/SmokeTest.java
@@ -109,7 +109,7 @@ public class SmokeTest {
 
         int result = new JFRTestUtil().run(jenkinsfile);
         assertThat("JFR should fail when there is no Jenkinsfile", result, not(equalTo(0)));
-        assertThat(systemOut.getLog(), containsString("No such file or directory"));
+        assertThat(systemOut.getLog(), containsString("FileNotFoundException"));
     }
 
     // TODO: uncomment once JFR can do something about timeouts internally
